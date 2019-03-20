627f34243fbe47f9c8307b8229baf2ed39e55503
julcsi <ejulcsi@gmail.com>
2019-03-20 23:36:49 +0100

    single page version

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-const sections = document.querySelectorAll('[data-section]')
-const menuButtons = document.querySelectorAll('.menu-link')
-
-function showPanel (id) {
-  sections.forEach(section => {
-    if (section.getAttribute('id') !== id) {
-      section.style.display = 'none'
-    } else {
-      section.style.display = 'block'
-    }
-  })
-}
-
-showPanel('about')
-
-menuButtons.forEach((button) => {
-  button.addEventListener('click', (e) => {
-
-    const href = e.target.getAttribute('href').slice(1)
-
-    showPanel(href)
-    e.preventDefault()
-  })
-})
-
 const menu = document.getElementById('menu')
 let pos = 0
 
