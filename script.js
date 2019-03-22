e0de7dac54b35c14de51d275ebc2e3d844554330
julcsi <ejulcsi@gmail.com>
2019-03-22 14:19:26 +0100

    easy desktop menu

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
 const menu = document.getElementById('menu')
-const submenu = document.getElementById('submenu')
-let pos = 0
 
-const FIXED = 'fixed-menu'
-const VISIBLE = 'visible'
 const LIFTED = 'lifted'
 
 
-window.addEventListener('scroll', () => {
-  const menubox = menu.getBoundingClientRect()
-  if (menubox.top < 0) {
-    pos = window.pageYOffset
-    setTimeout(() => {
-      document.body.classList.add(FIXED)
-    }, 500)
-  }
-
-  if (window.pageYOffset < pos - 20 && pos > 0) {
-    document.body.classList.remove(FIXED)
-    submenu.classList.remove(VISIBLE)
-  }
-})
-
-submenu.addEventListener('transitionend', (e) => {
-  if (e.propertyName === 'margin-top' && document.body.classList.contains(FIXED)) {
-    submenu.classList.add(VISIBLE)
-  }
-})
-
-const menulinks = menu.querySelectorAll('li a')
+const menulinks = menu.querySelectorAll('#menu a')
 
 menulinks.forEach(link => {
   link.addEventListener('click', e => {
